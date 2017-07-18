44194e2d7d1c74ad0b533df683a103ae5b5e17a5
Ryan <weegeemus@gmail.com>
2017-07-18 17:19:41 -0400

    Match.java wasn't included in the previous commit :(

---
+++ core/src/ryan/game/competition/Match.java
@@ -0,0 +1,79 @@
+package ryan.game.competition;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import ryan.game.Main;
+import ryan.game.Utils;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Match implements Serializable {
+
+    public final int number;
+    public final MatchAlliance red, blue;
+    public List<Integer> blueSurrogates = new ArrayList<>();
+    public List<Integer> redSurrogates = new ArrayList<>();
+    public boolean qualifier = true;
+    public String level = "qm"; //qm, qf, sf, f
+
+    public Match(int number, int[] b, int[] r) {
+        this.number = number;
+        blue = new MatchAlliance(b);
+        red = new MatchAlliance(r);
+    }
+
+    public void setBlueSurrogates(List<Integer> indexes) {
+        blueSurrogates = indexes;
+    }
+
+    public void setRedSurrogates(List<Integer> indexes) {
+        redSurrogates = indexes;
+    }
+
+    @Override
+    public String toString() {
+        String blueString = "", redString = "";
+        for (int i=0; i<blue.teams.length; i++) {
+            int t = blue.teams[i];
+            boolean surrogate = blueSurrogates.contains(i);
+            blueString += t + (surrogate ? "*" : "") + (i+1 != blue.teams.length ? ", " : "");
+        }
+
+        for (int i=0; i<red.teams.length; i++) {
+            int t = red.teams[i];
+            boolean surrogate = redSurrogates.contains(i);
+            redString += t + (surrogate ? "*" : "") + (i+1 != red.teams.length ? ", " : "");
+        }
+
+        return "Match " + number + ": " + blueString + "  vs  " + redString;
+    }
+
+    public void save() {
+        Gson g = new GsonBuilder().setPrettyPrinting().create();
+        Utils.writeFile("matches/" + Main.eventKey + "_" + level + "_" + number, g.toJson(this));
+    }
+
+    public static Match loadMatch(String level, int number) {
+        return loadMatch(Main.eventKey, level, number);
+    }
+
+    public static Match loadMatch(String event, String level, int number) {
+        //TODO
+        return null;
+    }
+
+    public class MatchAlliance implements Serializable {
+        public int[] teams;
+        public int score = 0;
+        public boolean winner = false;
+        public Object breakdown = null;
+
+        public MatchAlliance(int[] teams) {
+            this.teams = teams;
+        }
+    }
+}
+
+
